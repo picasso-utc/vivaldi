9559b914cce6a96487331d11724ae86e76724849
Unknown <josselin.pennors@hotmail.fr>
2019-08-12 00:55:07 +0200

    Add Admin Team Management Component

---
+++ src/components/admin/administrations/TeamManagement.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+class TeamManagement extends Component{
+ 
+    
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.container}>
+
+            </div>
+        );
+    };
+
+}
+
+const styles = theme => ({
+    container: {
+        padding: 50,
+    },
+});
+
+export default withStyles (styles) (TeamManagement)
