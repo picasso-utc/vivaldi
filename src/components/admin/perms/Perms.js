b556c71ea92dca545117be70ad6cbc0f84c7d24b
Unknown <josselin.pennors@hotmail.fr>
2019-08-12 00:57:08 +0200

    Add Admin Perms Manangement Component

---
+++ src/components/admin/perms/Perms.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+class Perms extends Component{
+ 
+    
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.container}>
+
+            </div>
+        );
+    };
+
+}
+
+const styles = theme => ({
+    container: {
+        padding: 50,
+    },
+});
+
+export default withStyles (styles) (Perms)
