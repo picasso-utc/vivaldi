f4e57f612e83827868a95791cfcd8f659c8a7a97
Unknown <josselin.pennors@hotmail.fr>
2019-08-12 15:04:59 +0200

    Create librabry AJAX to deal with Http Request

---
+++ src/utils/Ajax.js
@@ -0,0 +1,29 @@
+/**
+ * Sends an HTTP request to the API.
+ */
+
+import {API_URL} from './Config'
+import axios from 'axios';
+
+
+const defaultConfig = {withCredentials: true}
+
+
+function ajaxGet(path) {
+    return axios.get(API_URL + path, defaultConfig);
+}
+
+function ajaxPost(path, data) {
+    return axios.post(API_URL + path, data, defaultConfig);
+}
+
+function ajaxPut(path, data) {
+    return axios.put(API_URL + path, data, defaultConfig);
+}
+
+function ajaxDelete(path) {
+    return axios.delete(API_URL + path, defaultConfig);
+}
+
+
+export { ajaxGet, ajaxPost, ajaxPut, ajaxDelete };
