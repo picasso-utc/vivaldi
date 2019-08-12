169e7a3294971d1c410609a42be6f586e3b9b45c
Unknown <josselin.pennors@hotmail.fr>
2019-08-13 00:35:56 +0200

    Add User table + work on css style

--- src/components/admin/administrations/Users.js
+++ src/components/admin/administrations/Users.js
@@ -1,23 +1,20 @@
 import React, {Component} from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
-
-
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-
-
 import Grid from '@material-ui/core/Grid';
+
+import ajaxPost from '../../../utils/Ajax';
 
 class Users extends Component{
  
@@ -30,13 +27,15 @@ class Users extends Component{
                 login: '',
                 right: 'M'
             },
-    }
+            page: 0,
+            rowsPerPage: 5,
+        }
 
         this.handleChange = this.handleChange.bind(this);
         this.saveUser = this.saveUser.bind(this);
         this.handleChangePage = this.handleChangePage.bind(this);
     }
-        
+
     handleChange(event){
         this.setState({
             new_user: {
@@ -82,24 +81,21 @@ class Users extends Component{
             {id: 'jp4ennors', name: 'Josselin Pennors (jpennors)', right: 'M'},
             {id: 'auv5inali', name: 'Alix Auvin (auvinali)', right: 'M'},
         ];
-          
 
         return (
             <div className={classes.container}>
                 <Typography variant="h5" noWrap className={classes.subTitle}>
-                    <TrendingFlatIcon className={classes.subTitleIcon}/>
+                    <ChevronRightIcon className={classes.subTitleIcon}/>
                     Ajouter un nouvel utilisateur
                 </Typography>
-                <Grid container className={classes.note}>
-                    {/* <Typography variant="body2" > */}
+                {/* <Grid container className={classes.note}>
                         Droits :<br/>
-                        {/* </Typography> */}
                         <ul>
                             <li>Pas d'accès : Pas d'accès à Picsous.</li>
                             <li>User : Peut ajouter et modifier une perm, et gérer les articles de la perm.</li>
                             <li>Admin : Peut modifier l'ensemble des paramètres et gérer l'ensemble des factures.</li>
                         </ul>
-                </Grid>
+                </Grid> */}
                 <Grid container>
                     <Grid item xs={12} sm={5}>
                         <TextField
@@ -141,99 +137,73 @@ class Users extends Component{
                 </Grid>
                 
                 <Typography variant="h5" noWrap className={classes.subTitle}>
-                    <TrendingFlatIcon className={classes.subTitleIcon}/>
+                    <ChevronRightIcon className={classes.subTitleIcon}/>
                     Liste des utilisateurs
                 </Typography>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            {headRows.map(row => (
-                                <TableCell
-                                    key={row.id}
-                                    align={row.numeric ? 'right' : 'left'}
-                                    padding={row.disablePadding ? 'none' : 'default'}
-                                    // sortDirection={orderBy === row.id ? order : false}
-                                >
-                                    <TableSortLabel
-                                    // active={orderBy === row.id}
-                                    // direction={order}
-                                    // onClick={createSortHandler(row.id)}
-                                    >
-                                    {row.label}
-                                    {/* {orderBy === row.id ? (
-                                        <span className={classes.visuallyHidden}>
-                                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                                        </span>
-                                    ) : null} */}
-                                    </TableSortLabel>
-                                </TableCell>
-                            ))}
+                            <TableCell>
+                                Utilisateur
+                            </TableCell>
+                            <TableCell>
+                                Droit
+                            </TableCell>
                             <TableCell>
                                 Actions
                             </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {/* {rows.map((row, index) => (
-                            <TableRow
-                                hover
-                                // onClick={event => handleClick(event, row.name)}
-                                role="checkbox"
-                                // aria-checked={isItemSelected}
-                                tabIndex={-1}
-                                key={index}
-                                // selected={isItemSelected}
-                            >
-                                <TableCell component="th" scope="row" padding="none">
+                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row, index) => (
+                            <TableRow hover key={index} className={classes.row}>
+                                <TableCell component="th" scope="row" className={classes.cell}>
                                     {row.name}
                                 </TableCell>
-                                <TableCell component="th" scope="row" padding="none">
+                                <TableCell component="th" scope="row" className={classes.cell}>
                                     {row.right}
                                 </TableCell>
-                                <TableCell component="th" scope="row" padding="none">
-                                    Test
+                                <TableCell component="th" scope="row" className={classes.cell}>
+                                    {row.right == "A" && (
+                                        <Button variant="outlined" size="small" className={classes.btn} onClick={this.saveUser}>
+                                            Rétrograder
+                                        </Button>
+                                    )}
+                                    {row.right == "M" && (
+                                        <Button variant="outlined" size="small" color="primary" className={classes.btn} onClick={this.saveUser}>
+                                            Upgrader
+                                        </Button>
+                                    )}
+                                    {row.right == "N" ? (
+                                        <Button variant="outlined" size="small" color="primary" className={classes.btn} onClick={this.saveUser}>
+                                            Accès
+                                        </Button>
+                                    ): (
+                                        <Button variant="outlined" size="small" color="secondary" className={classes.btn} onClick={this.saveUser}>
+                                            Supprimer
+                                        </Button>
+                                    )}
+                                    
                                 </TableCell>
                             </TableRow>
-                        ))} */}
-                        {/* {stableSort(rows, getSorting(order, orderBy))
-                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        .map((row, index) => {
-                        const isItemSelected = isSelected(row.name);
-                        const labelId = `enhanced-table-checkbox-${index}`; */}
-
-                        {/* return (
-                            <TableRow
-                            hover
-                            onClick={event => handleClick(event, row.name)}
-                            role="checkbox"
-                            aria-checked={isItemSelected}
-                            tabIndex={-1}
-                            key={row.name}
-                            selected={isItemSelected}
-                            >
-                            <TableCell padding="checkbox">
-                                <Checkbox
-                                checked={isItemSelected}
-                                inputProps={{ 'aria-labelledby': labelId }}
-                                />
-                            </TableCell>
-                            <TableCell component="th" id={labelId} scope="row" padding="none">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
-                            </TableRow>
-                        );
-                        })} */}
-                    {/* {emptyRows > 0 && (
-                        <TableRow style={{ height: 49 * emptyRows }}>
-                        <TableCell colSpan={6} />
-                        </TableRow>
-                    )} */}
+                        ))}
                     </TableBody>
                 </Table>
+                <TablePagination
+                    component="div"
+                    rowsPerPageOptions={[]}
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    backIconButtonProps={{
+                        'aria-label': 'previous page',
+                    }}
+                    nextIconButtonProps={{
+                        'aria-label': 'next page',
+                    }}
+                    onChangePage={this.handleChangePage}
+                />
             </div>
         );
     };
@@ -250,30 +220,41 @@ const styles = theme => ({
     paper: {
         padding: 10
     },
-    note: {
-        backgroundColor: 'rgba(0,0,0, 0.05)',
-        padding: 10
-    },
+    // note: {
+    //     backgroundColor: 'rgba(0,0,0, 0.05)',
+    //     padding: 10
+    // },
     textField: {
         marginTop: 16,
         paddingRight: 15,
         width: "100%"
     },
-    button: {
+    addButton: {
         marginTop: 16,
         marginBottom: 8,
         height: 49,
         width: "100%",
-        borderColor: '#B22132',
-        color: '#B22132',
     },
     subTitle: {
-        marginTop: 16,
-        marginBottom: 16,
+        marginTop: 10,
+        marginBottom: 10,
     },
     subTitleIcon: {
         marginRight: 8,
-    }
+        paddingTop: 5,
+    },
+    row: {
+        height: 40,
+    },
+    cell: {
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    btn: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 3,
+    },
 });
 
 export default withStyles (styles) (Users)
