402c2d70b4690eac4d3472d25ae26a6b8c2470f6
Unknown <josselin.pennors@hotmail.fr>
2019-08-12 22:20:33 +0200

    Review the navbar and the cover picture, make it responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,87 +6,52 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import Payment from '@material-ui/icons/Payment';
 import { withStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Icon } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Hidden from '@material-ui/core/Hidden';
 
-const HEADER_PATH = '/images/header_p19.png'
-const HEADER_IMG = '/images/header_pic.jpg'
+
+import CreditCardIcon from '@material-ui/icons/CreditCard';
+import LockIcon from '@material-ui/icons/Lock';
+
+const COUVERTURE_PATH = '/images/header_pic.jpg'
+const LOGO_PATH = '/images/header_p19.png'
 
 class Header extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			showMenu: false,
-			width: 0,
-			heigth: 0,
-		};
-	}
-
-	componentDidMount() {
-		this.updateWindowDimensions();
-		//window.addEventListener('resize', this.updateWindowDimensions);
-	}
-	  
-	componentWillUnmount() {
-		//window.removeEventListener('resize', this.updateWindowDimensions);
-	}
-	  
-	updateWindowDimensions() {
-		this.setState({ width: window.innerWidth, height: window.innerHeight });
-	}
-
-	toggleMenu = () => {
-
 	}
 
 	render() {
-		const { classes } = this.props;
-		return (
-			<div>
-			{/* <AppBar position="fixed" color="secondary"> */}
-				{/* <Toolbar className={classes.Toolbar} style={{ minHeight: this.props.height }}>
-					<Link to="/">
-						<img src={HEADER_PATH} alt="Foyer Pic'asso" className={classes.logo}/>
-					</Link>
 
-					<div>
-						<IconButton href="#Calendar" className={classes.menuButton} aria-label="Calendar">
-							<Icon className={classes.centerIcon}> calendar_today</Icon>
-						</IconButton>
-						<IconButton href="#News" className={classes.menuButton} aria-label="News">
-							<Icon className={classes.centerIcon}> description</Icon>
-						</IconButton>
-						<IconButton href="#LeSaviezVous" className={classes.menuButton} aria-label="Faq">
-							<Icon className={classes.centerIcon}> bookmark</Icon>
-						</IconButton>
-						<IconButton href="#calendar" className={classes.menuButton} aria-label="Goodies">
-							<Icon className={classes.centerIcon}> card_giftcard</Icon>
-						</IconButton>
-						<IconButton href="#calendar" className={classes.menuButton} aria-label="Prices">
-							<Icon className={classes.centerIcon}> euro_symbol</Icon>
-						</IconButton>
-						<IconButton href="#calendar" className={classes.menuButton} aria-label="Team">
-							<Icon className={classes.centerIcon}> photo</Icon>
-						</IconButton>
-					</div>
-					<div>
-						{this.showMenu && (
-							<IconButton className={classes.menuButton} aria-label="Menu" onClick={this.toggleMenu}>
-								<Icon>menu</Icon>
-							</IconButton>
-						)}
-						
-						<IconButton href="https://payutc.nemopay.net" className={classes.menuButton} color="inherit" aria-label="Payutc" >
-							<Payment className={classes.leftIcon} />payutc
-						</IconButton>
-						<IconButton to='/login' component={NavLink} className={classes.menuButton} color="inherit" aria-label="Login">
-							<AccountCircle className={classes.leftIcon} />login
-     				</IconButton>
-					</div>
-				</Toolbar> */}
-			{/* </AppBar> */}
-			<div className = {classes.imgContainer}>
-				<h1 className={classes.headerTitle}></h1>
-			</div>
-			</div>
+		const { classes } = this.props;
+
+		return (
+			<React.Fragment>
+				<div className={classes.root}>
+					<AppBar position="fixed" className={classes.appBar}>
+						<Toolbar>
+							<div className={classes.logo}>
+								<img src={LOGO_PATH} height="60px"/>
+							</div>
+							
+							<Hidden xsDown implementation="css">
+
+								<IconButton href="https://payutc.nemopay.net" target="_blank" className={classes.menuButton} aria-label="Calendar">
+									<CreditCardIcon className={classes.icon}/>
+								</IconButton>
+								<IconButton href="/login" className={classes.menuButton} aria-label="Login">
+									<LockIcon className={classes.icon}/>
+								</IconButton>
+							</Hidden>
+						</Toolbar>
+					</AppBar>
+				</div>
+				<div className = {classes.imgContainer}>
+					<img src={COUVERTURE_PATH} className={classes.couverture}/>
+				</div>
+			</React.Fragment>
 		);
 
 	}
@@ -97,58 +62,44 @@ Header.propTypes = {
 };
 
 const styles = theme => ({
-	Toolbar: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		maxWidth: 1280,
-		width: '100%',
-		marginLeft: 'auto',
-		marginRight: 'auto',
-		marginTop: 0,
+	root: {
+		flexGrow: 1,
+	},
+	appBar: {
+		backgroundColor: "white",
+		color: '#000223',
+		
 	},
 	logo: {
-		maxHeigth: 70,
-		maxWidth: 180,
+		flexGrow: 1,
 	},
 	menuButton: {
-		color: 'inherit',
-		fontSize: 20,
+		color: '#B22132',
+		fontSize: 35,
 		padding: "10px",
 		marginTop: 0,
 		borderRadius: 0,
-		height: "100%",
+		height: 60,
 		
 		'&:hover': {
-			borderTop: "3px solid red",
+			borderTop: "3px solid #B22132",
 			textDecoration: "none",
 		},
-		'&:active': {
-			borderTop: "3px solid red",
-			textDecoration: "none",
-		}
 	},
-	leftIcon: {
-		// marginRight: theme.spacing.unit,
-		fontSize: 30,
-	},
-	centerIcon: {
+	icon: {
 		fontSize: 35,
-		// marginRight: theme.spacing.unit,
-		// marginLeft: theme.spacing.unit,	
 	},
 	imgContainer: {
-		width: "100%",
-		height: window.innerHeight - 64,
-		backgroundImage: "url(" + HEADER_IMG + ")",
-		backgroundRepeat: "no-repeat",
-		backgroundPosition: "center",
-		backgroundSize: "cover",
-		marginTop: "64px",
+		width: '100%',
+		position: 'absolute',
+		right: 0,
+		left: 'auto',
+		marginTop: 65,
 	},
-	headerTitle: {
-		fontSize: "40px",
-		textAlign: "center",
+	couverture: {
+		width: '100%',
+		height: window.innerHeight - (65),
+		objectFit: 'fill',
 	},
 });
 
