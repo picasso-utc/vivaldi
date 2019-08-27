d6217551364648f58cd3876bcf82ba9d1d549667
Unknown <josselin.pennors@hotmail.fr>
2019-08-28 00:27:10 +0200

    Review Goodies Displayer design

--- src/components/home/GoodiesDisplayer.js
+++ src/components/home/GoodiesDisplayer.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 import { ajaxGet } from '../../utils/Ajax';
+import { List, ListItem, ListItemText } from "@material-ui/core";
 
 class GoodiesDisplayer extends React.Component {
   
@@ -33,14 +36,43 @@ class GoodiesDisplayer extends React.Component {
 
 		const { winners } = this.state;
 
+		const { classes } = this.props;
+
     	return (
 			<React.Fragment>
 				{winners.length == 0 ? (
-					<p>Pas de goodies</p>
+					// <p>Pas de goodies</p>
+					<div></div>
 				):(
-					winners.map((winner, index) => (
-						<p key={index}>{winner.winner}</p>
-					))
+					<Grid container
+						direction="row"
+					>
+						<Grid item xs={6}>
+							<List dense={true}>
+								{winners.slice(0,Math.ceil(winners.length/2))
+								.map((winner, index) => (
+									<ListItem key={index} className={classes.item}>
+										<ListItemText 
+											primary={winner.winner} $
+											className={winner.picked_up && classes.validatedItem}/>
+									</ListItem>
+								))}
+							</List>
+						</Grid>
+						<Grid item xs={6}>
+							<List dense={true}>
+								{winners.slice(Math.ceil(winners.length/2), winners.length)
+								.map((winner, index) => (
+									<ListItem key={index} className={classes.item}>
+										<ListItemText 
+											primary={winner.winner} 
+											className={winner.picked_up && classes.validatedItem}
+										/>
+									</ListItem>
+								))}
+							</List>
+						</Grid>
+					</Grid>
 				)}
 			</React.Fragment>
     	);
@@ -48,7 +80,13 @@ class GoodiesDisplayer extends React.Component {
 }
 
 const styles = theme => ({
-
+	item : {
+		textAlign: 'center',
+		fontSize: 16,
+	},
+	validatedItem: {
+		textDecoration: 'line-through',
+	},
 });
 
 export default withStyles (styles) (GoodiesDisplayer)
