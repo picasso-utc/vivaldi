6942616785d4dece788b5fe1f70cb53454a6ddae
Unknown <josselin.pennors@hotmail.fr>
2019-08-27 22:38:46 +0200

    Goodies and Price Displayer, API Call + Display it

--- src/components/home/GoodiesDisplayer.js
+++ src/components/home/GoodiesDisplayer.js
@@ -1,17 +1,47 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles'
 
+import { ajaxGet } from '../../utils/Ajax';
+
 class GoodiesDisplayer extends React.Component {
   
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			winners: []
+		}
+	}
+
+	
+	componentDidMount(){
+		this.loadGoodiesWinners();
+	}
+
+	
+	loadGoodiesWinners(){
+        ajaxGet('payutc/goodies').then(res => {
+            this.setState({winners: res.data.winners});
+        })
+        .catch(error => {
+            console.log(error)
+            this.setState({loading: false})
+        })
     }
 
   	render() {
+
+		const { winners } = this.state;
+
     	return (
 			<React.Fragment>
-
+				{winners.length == 0 ? (
+					<p>Pas de goodies</p>
+				):(
+					winners.map((winner, index) => (
+						<p key={index}>{winner.winner}</p>
+					))
+				)}
 			</React.Fragment>
     	);
   	}
--- src/components/home/PriceDisplayer.js
+++ src/components/home/PriceDisplayer.js
@@ -1,16 +1,86 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles'
 
+import { ajaxGet } from '../../utils/Ajax';
+
 class PriceDisplayer extends React.Component {
   
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			articles : {}
+		}
+	}
+	
+	componentDidMount(){
+		this.loadArticles();
+	}
+
+	loadArticles(){
+        ajaxGet('payutc/public/articles').then(res => {
+            this.setState({articles: res.data});
+        })
+        .catch(error => {
+            console.log(error)
+            this.setState({loading: false})
+        })
     }
 
   	render() {
+
+		const { articles } = this.state;
+
+
     	return (
 			<React.Fragment>
+				{'softs' in articles && (
+					articles.softs.map((soft, index) => (
+						<p key={index}>{soft.name} {soft.price}</p>
+					))
+				)}
+
+				{'bieresBouteille' in articles && (
+					articles.bieresBouteille.map((biereBouteille, index) => (
+						<p key={index}>{biereBouteille.name} {biereBouteille.price}</p>
+					))
+				)}
+
+				{'snacksSucres' in articles && (
+					articles.snacksSucres.map((snackSucre, index) => (
+						<p key={index}>{snackSucre.name} {snackSucre.price}</p>
+					))
+				)}
+
+				{'snacksSales' in articles && (
+					articles.snacksSales.map((snackSale, index) => (
+						<p key={index}>{snackSale.name} {snackSale.price}</p>
+					))
+				)}
+
+				{'glace' in articles && (
+					articles.glace.map((g, index) => (
+						<p key={index}>{g.name} {g.price}</p>
+					))
+				)}
+
+				{'petitDej' in articles && (
+					articles.petitDej.map((p, index) => (
+						<p key={index}>{p.name} {p.price}</p>
+					))
+				)}
+
+				{'pampryls' in articles && (
+					articles.pampryls.map((pampryl, index) => (
+						<p key={index}>{pampryl.name} {pampryl.price}</p>
+					))
+				)}
+
+				{'bieresPression' in articles && (
+					articles.bieresPression.map((bierePression, index) => (
+						<p key={index}>{bierePression.name} {bierePression.price}</p>
+					))
+				)}
 
 			</React.Fragment>
     	);
