e9d0caf3a893b21fd65f589ad5e367ce212f4460
Unknown <josselin.pennors@hotmail.fr>
2019-09-02 16:43:29 +0200

    Rearrange Price displayer style

--- src/components/home/PriceDisplayer.js
+++ src/components/home/PriceDisplayer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid';
 
 import { ajaxGet } from '../../utils/Ajax';
 
@@ -31,64 +32,185 @@ class PriceDisplayer extends React.Component {
 
 		const { articles } = this.state;
 
+		const { classes } = this.props;
+
 
     	return (
 			<React.Fragment>
-				{'softs' in articles && (
-					articles.softs.map((soft, index) => (
-						<p key={index}>{soft.name} {soft.price}</p>
-					))
-				)}
-
-				{'bieresBouteille' in articles && (
-					articles.bieresBouteille.map((biereBouteille, index) => (
-						<p key={index}>{biereBouteille.name} {biereBouteille.price}</p>
-					))
-				)}
-
-				{'snacksSucres' in articles && (
-					articles.snacksSucres.map((snackSucre, index) => (
-						<p key={index}>{snackSucre.name} {snackSucre.price}</p>
-					))
-				)}
-
-				{'snacksSales' in articles && (
-					articles.snacksSales.map((snackSale, index) => (
-						<p key={index}>{snackSale.name} {snackSale.price}</p>
-					))
-				)}
-
-				{'glace' in articles && (
-					articles.glace.map((g, index) => (
-						<p key={index}>{g.name} {g.price}</p>
-					))
-				)}
-
-				{'petitDej' in articles && (
-					articles.petitDej.map((p, index) => (
-						<p key={index}>{p.name} {p.price}</p>
-					))
-				)}
-
-				{'pampryls' in articles && (
-					articles.pampryls.map((pampryl, index) => (
-						<p key={index}>{pampryl.name} {pampryl.price}</p>
-					))
-				)}
-
-				{'bieresPression' in articles && (
-					articles.bieresPression.map((bierePression, index) => (
-						<p key={index}>{bierePression.name} {bierePression.price}</p>
-					))
-				)}
-
+				<Grid container direction="row">
+					<Grid item xs={3}>
+						<h2>Softs</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'softs' in articles && (
+									articles.softs.map((soft, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{soft.name}
+											</td>
+											<td className={classes.article_price}>
+												{(soft.price/100).toFixed(2)}€
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+					<Grid item xs={3}>
+						<h2>Pampryls</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'pampryls' in articles && (
+									articles.pampryls.map((pampryl, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{pampryl.name}
+											</td>
+											<td className={classes.article_price}>
+												{(pampryl.price/100).toFixed(2)} €
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+					<Grid item xs={3}>
+						<h2>Bières Bouteilles</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'bieresBouteille' in articles && (
+									articles.bieresBouteille.map((biereBouteille, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{biereBouteille.name}
+											</td>
+											<td className={classes.article_price}>
+												{(biereBouteille.price/100).toFixed(2)} €
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+					<Grid item xs={3}>
+						<h2>Bières Pression</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'bieresPression' in articles && (
+									articles.bieresPression.map((bierePression, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{bierePression.name}
+											</td>
+											<td className={classes.article_price}>
+												{(bierePression.price/100).toFixed(2)} €
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+				</Grid>
+				<Grid container direction="row">
+					<Grid item xs={3}>
+						<h2>Snacks Salés</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'snacksSales' in articles && (
+									articles.snacksSales.map((snackSale, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{snackSale.name}
+											</td>
+											<td className={classes.article_price}>
+												{(snackSale.price/100).toFixed(2)} €
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+					<Grid item xs={3}>
+						<h2>Snacks Sucrés</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'snacksSucres' in articles && (
+									articles.snacksSucres.map((snackSucre, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{snackSucre.name}
+											</td>
+											<td className={classes.article_price}>
+												{(snackSucre.price/100).toFixed(2)} €
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+					<Grid item xs={3}>
+						<h2>P'tit Dej</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'petitDej' in articles && (
+									articles.petitDej.map((p, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{p.name}
+											</td>
+											<td className={classes.article_price}>
+												{(p.price/100).toFixed(2)} €
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+					<Grid item xs={3}>
+						<h2>Glace</h2>
+						<table className={classes.table}>
+							<tbody>
+								{'glace' in articles && (
+									articles.glace.map((g, index) => (
+										<tr key={index}>
+											<td className={classes.article_name}>
+												{g.name}
+											</td>
+											<td className={classes.article_price}>
+												{(g.price/100).toFixed(2)} €
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</table>
+					</Grid>
+				</Grid>
 			</React.Fragment>
     	);
   	}
 }
 
 const styles = theme => ({
-
+	table : {
+		borderLeft: '1.5px solid #B22132',
+	},
+	article_name : {
+		paddingLeft: 5,
+		paddingRight: 5,
+	},
+	article_price : {
+		paddingRight: 15,
+		paddingLeft: 5,
+		whiteSpace: 'nowrap',
+	},
 });
 
 export default withStyles (styles) (PriceDisplayer)
