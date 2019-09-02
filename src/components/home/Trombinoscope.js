50a09d1dd630cbd40b800b8937e69e98c198372f
Unknown <josselin.pennors@hotmail.fr>
2019-09-02 22:32:25 +0200

    Calendar picture + WIP Calendar

--- src/components/home/Trombinoscope.js
+++ src/components/home/Trombinoscope.js
@@ -17,86 +17,107 @@ class Trombinoscope extends React.Component {
 			{
 				nom: 'Ayman Lamdasni',
 				poste: 'Co-Président',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Alix Auvin',
 				poste: 'Co-Président',
+				image: '/images/alix.png',
 			},
 			{
 				nom: 'Pauline Mandon',
 				poste: 'Trésorière',
+				image: '/images/pauline.png',
 			},
 			{
 				nom: 'Léo Tavernier',
 				poste: 'Vice-Trésorier',
+				image: '/images/leo.png',
 			},
 			{
 				nom: 'Mathilde Petit',
-				poste: 'Resp Anim'
+				poste: 'Resp Anim',
+				image: '/images/mathilde.png',
 			},
 			{
 				nom: 'Maliana Hoatau',
 				poste: 'Team Anim',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Nicolas Théron',
 				poste: 'Team Anim',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Constant Dassonville',
 				poste: 'Team Anim',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Oubine Perrin',
 				poste: 'Team Anim',
+				image: '/images/oubine.png',
 			},
 			{
 				nom: 'Félix Roulleaux Dugage',
-				poste: 'Resp Com'
+				poste: 'Resp Com',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Reda Sarehane',
 				poste: 'Team Com',
+				image: '/images/reda.png',
 			},
 			{
 				nom: 'Florestan Biaux',
 				poste: 'Team Com',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Ariane Lozon',
 				poste: 'Team Locaux',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Léo Romanet',
 				poste: 'Team Locaux',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Yanis Aboura',
 				poste: 'Team Locaux',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Alexandre Brasseur',
 				poste: 'Appro Bières',
+				image: '/images/brass.png',
 			},
 			{
 				nom: 'Jessica Guilbaud',
 				poste: 'Appro Softs',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Rémy Dudzik',
 				poste: 'Appro Softs',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Corentin Martin',
 				poste: 'Appro Softs',
+				image: '/images/ayman.png',
 			},
 			{
 				nom: 'Josselin Pennors',
 				poste: 'Resp Info',
+				image: '/images/josselin.png',
 			},
 			{
 				nom: 'Capucine Martin',
 				poste: 'Team Info',
+				image: '/images/ayman.png',
 			},
 		]
     	return (
@@ -106,12 +127,12 @@ class Trombinoscope extends React.Component {
 						<Grid xs={4} sm={3} item className={classes.card} key={index}>
 							<div className={classes.card_content}>
 								<div className={classes.card_img}>
-									<img src="/images/axel.png" className={classes.img}/>
+									<img src={member.image} className={classes.img}/>
 								</div>
 								<div className={classes.card_description}>
 									<div style={{alignItems:'center'}}>
-										<h3>{member.nom}</h3>
-										<h5>{member.poste}</h5>
+										<h2>{member.nom}</h2>
+										<h3>{member.poste}</h3>
 									</div>
 								</div>
 							</div>
@@ -133,10 +154,6 @@ const styles = theme => ({
 		margin: '.5rem 0 1rem 0',
 		borderRadius: 2,
 		boxShadow: '0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2)',
-	},
-	card_img : {
-		// margin: 20,
-		// position: 'relative',
 	},
 	img: {
 		width: '100%',
