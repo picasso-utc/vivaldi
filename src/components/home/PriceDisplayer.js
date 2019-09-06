cecb84cec3fb17d3f4162ef7e168391348b730fe
Unknown <josselin.pennors@hotmail.fr>
2019-09-06 14:36:17 +0200

    Refactor code to display articles + wip responsive style

--- src/components/home/PriceDisplayer.js
+++ src/components/home/PriceDisplayer.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid';
-
 import { ajaxGet } from '../../utils/Ajax';
+import Hidden from '@material-ui/core/Hidden';
+
 
 class PriceDisplayer extends React.Component {
   
@@ -35,165 +36,80 @@ class PriceDisplayer extends React.Component {
 		const { classes } = this.props;
 
 
+		const pic_articles = [
+			{type: 'boisons', content : [
+				{name: 'Softs', code: 'softs'},
+				{name: 'Pampryls', code: 'pampryls'},
+				{name: 'Bières Bouteilles', code: 'bieresBouteille'},
+				{name: 'Bières Pression', code: 'bieresPression'},
+			]},
+			{type: 'snacks', content : [
+				{name: 'Snacks Salés', code: 'snacksSales'},
+				{name: 'Snacks Sucrés', code: 'snacksSucres'},
+				{name: 'P\'tit Dej', code: 'petitDej'},
+				{name: 'Glaces', code: 'glace'},
+			]}
+		]
+
+
     	return (
 			<React.Fragment>
-				<Grid container direction="row">
-					<Grid item xs={3}>
-						<h2>Softs</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'softs' in articles && (
-									articles.softs.map((soft, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{soft.name}
-											</td>
-											<td className={classes.article_price}>
-												{(soft.price/100).toFixed(2)}€
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-					<Grid item xs={3}>
-						<h2>Pampryls</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'pampryls' in articles && (
-									articles.pampryls.map((pampryl, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{pampryl.name}
-											</td>
-											<td className={classes.article_price}>
-												{(pampryl.price/100).toFixed(2)} €
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-					<Grid item xs={3}>
-						<h2>Bières Bouteilles</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'bieresBouteille' in articles && (
-									articles.bieresBouteille.map((biereBouteille, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{biereBouteille.name}
-											</td>
-											<td className={classes.article_price}>
-												{(biereBouteille.price/100).toFixed(2)} €
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-					<Grid item xs={3}>
-						<h2>Bières Pression</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'bieresPression' in articles && (
-									articles.bieresPression.map((bierePression, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{bierePression.name}
-											</td>
-											<td className={classes.article_price}>
-												{(bierePression.price/100).toFixed(2)} €
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-				</Grid>
-				<Grid container direction="row">
-					<Grid item xs={3}>
-						<h2>Snacks Salés</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'snacksSales' in articles && (
-									articles.snacksSales.map((snackSale, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{snackSale.name}
-											</td>
-											<td className={classes.article_price}>
-												{(snackSale.price/100).toFixed(2)} €
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-					<Grid item xs={3}>
-						<h2>Snacks Sucrés</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'snacksSucres' in articles && (
-									articles.snacksSucres.map((snackSucre, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{snackSucre.name}
-											</td>
-											<td className={classes.article_price}>
-												{(snackSucre.price/100).toFixed(2)} €
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-					<Grid item xs={3}>
-						<h2>P'tit Dej</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'petitDej' in articles && (
-									articles.petitDej.map((p, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{p.name}
-											</td>
-											<td className={classes.article_price}>
-												{(p.price/100).toFixed(2)} €
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-					<Grid item xs={3}>
-						<h2>Glace</h2>
-						<table className={classes.table}>
-							<tbody>
-								{'glace' in articles && (
-									articles.glace.map((g, index) => (
-										<tr key={index}>
-											<td className={classes.article_name}>
-												{g.name}
-											</td>
-											<td className={classes.article_price}>
-												{(g.price/100).toFixed(2)} €
-											</td>
-										</tr>
-									))
-								)}
-							</tbody>
-						</table>
-					</Grid>
-				</Grid>
+				{pic_articles.map(({type, content}, index) => (
+					<div key={index}>
+						<Hidden mdUp implementation="css">
+							<Grid container direction="row" className={classes.responsive_displayer}>
+								{content.map((boisson, index) => (
+									<Grid item key={index} className={classes.responsive_displayer_item}>
+										<h2>{boisson.name}</h2>
+										<table className={classes.table}>
+											<tbody>
+												{boisson.code in articles && (
+													articles.softs.map((soft, article_index) => (
+														<tr key={article_index}>
+															<td className={classes.article_name}>
+																{soft.name}
+															</td>
+															<td className={classes.article_price}>
+																{(soft.price/100).toFixed(2)}€
+															</td>
+														</tr>
+													))
+												)}
+											</tbody>
+										</table>
+									</Grid>
+								))}
+							</Grid>
+						</Hidden>
+						<Hidden smDown implementation="css">
+							<Grid container direction="row">
+								{content.map((boisson, index) => (
+									<Grid item key={index} md={3}>
+										<h2>{boisson.name}</h2>
+										<table className={classes.table}>
+											<tbody>
+												{boisson.code in articles && (
+													articles.softs.map((soft, article_index) => (
+														<tr key={article_index}>
+															<td className={classes.article_name}>
+																{soft.name}
+															</td>
+															<td className={classes.article_price}>
+																{(soft.price/100).toFixed(2)}€
+															</td>
+														</tr>
+													))
+												)}
+											</tbody>
+										</table>
+									</Grid>
+								))}
+							</Grid>
+						</Hidden>
+					</div>
+				))}
 			</React.Fragment>
+			
     	);
   	}
 }
@@ -210,6 +126,17 @@ const styles = theme => ({
 		paddingRight: 15,
 		paddingLeft: 5,
 		whiteSpace: 'nowrap',
+	},
+	responsive_displayer: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		overflow: 'hidden',
+		flexWrap: 'nowrap',
+		transform: 'translateZ(0)',
+	},
+	responsive_displayer_item: {
+		marginLeft: 15,
 	},
 });
 
