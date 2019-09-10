f9c1bc3d209569999cdf006d6f5a68d7564bf8e4
Unknown <josselin.pennors@hotmail.fr>
2019-09-10 12:39:33 +0200

    Fix bug for production interceptor with path

--- src/utils/Interceptor.js
+++ src/utils/Interceptor.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import { asset_url } from './Config';
 
 axios.interceptors.response.use( (response) => {
     return response;
  }, (error) => {
     switch (error.response.status) {
         case 403:
-            if (window.location.pathname !== "/login") {
-                window.location.href = "/login";
+            if (window.location.pathname !== asset_url("/login")) {
+                window.location.href = asset_url("/login");
             }
             break;
         default:
