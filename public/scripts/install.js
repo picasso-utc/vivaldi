3a5e1c04654a3b4847266d50577276b2f53ba9d0
Unknown <josselin.pennors@hotmail.fr>
2019-09-28 18:25:44 +0200

    WIP PWA

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -1,11 +1,25 @@
-// CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+// // CODELAB: Add event listener for beforeinstallprompt event
+// window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
-// CODELAB: Add code to save event & show the install button.
-deferredInstallPrompt = evt;
-installButton.removeAttribute('hidden');
+// // CODELAB: Add code to save event & show the install button.
+// deferredInstallPrompt = evt;
+// installButton.removeAttribute('hidden');
 
-// CODELAB: Add code show install prompt & hide the install button.
-deferredInstallPrompt.prompt();
-// Hide the install button, it can't be called twice.
-evt.srcElement.setAttribute('hidden', true);
+// // CODELAB: Add code show install prompt & hide the install button.
+// deferredInstallPrompt.prompt();
+// // Hide the install button, it can't be called twice.
+// evt.srcElement.setAttribute('hidden', true);
+
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  installButton.removeAttribute('hidden');
+
+    // CODELAB: Add code show install prompt & hide the install button.
+    deferredPrompt.prompt();
+    // Hide the install button, it can't be called twice.
+    evt.srcElement.setAttribute('hidden', true);
+});
