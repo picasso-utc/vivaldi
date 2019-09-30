897348a2083df143f49766da4ba6f7a800d20a14
Unknown <josselin.pennors@hotmail.fr>
2019-09-30 08:46:36 +0200

    fix issue with Date utils, when formate to django date

--- src/utils/Date.js
+++ src/utils/Date.js
@@ -1,5 +1,5 @@
 export function formateToDjangoDate(date) {
-    const day = ("0" + (date.getDate() + 1)).slice(-2);
+    const day = ("0" + (date.getDate())).slice(-2);
     const month_number = ("0" + (date.getMonth() + 1)).slice(-2)
     const year = date.getFullYear();
     return year + "-" + month_number + "-" + day;
