7e94a389df25553634dee49702eb40c511d90b72
Capucine <capucine.martin@etu.utc.fr>
2019-10-22 17:32:22 +0200

    Create notation card

---
+++ src/components/admin/CarouselItem.js
@@ -0,0 +1,205 @@
+import React, {Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, TextField, Button, Grid, Menu, MenuItem, Paper } from '@material-ui/core';
+import { ChevronRight } from '@material-ui/icons';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { Card, CardContent, CardActions } from '@material-ui/core';
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
+import notation_perm from './notation.json'
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import withWidth from '@material-ui/core/withWidth';
+
+
+import { ajaxGet, ajaxPost } from '../../utils/Ajax';
+
+
+class CarouselItem extends Component{
+ 
+	
+	constructor(props) {
+		super(props)
+
+		this.state ={
+			creneaux : [],
+			notation : [],
+			grid_per_row : this.props.grid_per_row,
+			cell_height : this.props.cell_height,
+		}
+
+		
+
+		this.loadCreneau = this.loadCreneau.bind(this);
+	}
+
+	componentDidMount(){
+		this.loadCreneau();
+		console.log(notation_perm);
+		if(this.props.periode == "soir"){
+			this.setState({notation : notation_perm.soir});
+			console.log(this.notation)
+		}
+		if(this.props.periode == "midi"){
+			this.setState({notation : notation_perm.midi});
+		}
+		if(this.props.periode == "matin"){
+			this.setState({notation : notation_perm.matin});
+		}
+
+	}
+
+
+
+	loadCreneau(){
+		ajaxGet('perm/creneaux').then(res => {
+
+			this.setState({creneaux: res.data});
+		})
+	}
+
+	render(){
+		
+		const { classes, width } = this.props;
+		const { creneaux,  grid_per_row, notation, cell_height } = this.state;
+		let columns = width === 'xs' || width === 'sm'  ? 1 : 2;
+
+		return (
+			<div className={classes.gridList}>
+				{creneaux.map(creneau => (
+					<div key={creneau.id} cols={1} rows={1}>
+						<Card item xs={12} sm= {6} className={classes.card}>
+							<Typography variant="h5" noWrap className={classes.subTitle}>
+								{creneau.perm.nom} - {creneau.date}
+							</Typography>
+							<CardContent>
+								<FormControl component="fieldset"> 
+									<Grid container direction="row">   
+									    {notation.map(perm => (
+
+											<Grid item xs={12} sm= {6} >
+												<Typography variant="h6" noWrap className={classes.subTitle}>
+													{perm.categorie}
+												</Typography>
+												<RadioGroup aria-label="position" name="position" >
+													{perm.notation.map(menu => (
+											    		<FormControlLabel
+														value={menu.value}
+														control={<Radio color="primary" />}
+														label={menu.label}
+														/>
+													))}
+											    </RadioGroup>
+										    </Grid>
+										))}
+									</Grid>
+
+									<TextField
+										id="outlined-multiline-flexible"
+										label="Commentaire"
+										multiline
+										rowsMax="4"
+										//value={values.multiline}
+										// onChange={handleChange('multiline')}
+										className={classes.textField}
+										margin="normal"
+										variant="outlined"
+									/>
+								</FormControl>
+								<CardActions style={{justifyContent: 'center'}}>
+									<Button variant="contained" size="large" color="primary" >
+						        		Valider
+						        	</Button>
+							  	</CardActions>
+							</CardContent>
+						</Card>
+					</div>
+				))}
+			</div>
+		);
+	};
+
+}
+
+const styles = theme => ({
+	container: {
+		padding: 20,
+		margin: 30,
+		marginTop: 100,
+		border: "2px solid #B22132",
+	},
+	section:{
+		paddingBottom :70,
+	},
+	paper: {
+		padding: 10
+	},
+	note: {
+		backgroundColor: 'rgba(0,0,0, 0.05)',
+		padding: 10,
+		border: 'thin solid grey',
+		marginTop: 16,
+		marginBottom: 8,
+	},
+	textField: {
+		marginTop: 16,
+		paddingRight: 15,
+		width: "100%",
+	},
+	suggestions: {
+		zIndex: 100,
+		position: 'absolute',
+		maxHeight: 200,
+		overflowY: 'scroll',
+		marginRight: 15,
+	},
+	card: {
+		margin: 30,
+		padding : 10,
+		fontSize : 9
+	},
+	addButton: {
+		marginTop: 16,
+		marginBottom: 8,
+		height: 49,
+		width: "100%",
+	},
+	subTitle: {
+		marginTop: 10,
+		marginBottom: 10,
+	},
+	subTitleIcon: {
+		marginRight: 8,
+		paddingTop: 5,
+	},
+	row: {
+		height: 40,
+	},
+	cell: {
+		paddingTop: 10,
+		paddingBottom: 10,
+	},
+	btn: {
+		marginLeft: 5,
+		marginRight: 5,
+		marginTop: 3,
+	},
+	gridList: {
+		display: 'flex',
+		flexWrap: 'nowrap',
+		overflowX: 'auto',
+    	// Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    	transform: 'translateZ(0)',
+    },
+});
+
+export default withStyles (styles) (CarouselItem)
+
+
+
+
+
+
+
+
+
--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -1,33 +1,178 @@
-import React, {Component} from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import React, {Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, TextField, Button, Grid, Menu, MenuItem, Paper } from '@material-ui/core';
+import { ChevronRight } from '@material-ui/icons';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { Card, CardContent, CardActions } from '@material-ui/core';
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
+import notation_perm_soir from './notation.json'
+import CarouselItem from './CarouselItem.js'
+
+import { ajaxGet, ajaxPost } from '../../utils/Ajax';
+
 
 class Dashboard extends Component{
  
-    
-    constructor(props) {
-        super(props)
-    }
+	
+	constructor(props) {
+		super(props)
 
-    render(){
-        
-        const { classes } = this.props;
+		this.state ={
+			creneaux : [],
+		}
+		
 
-        return (
-            <div className={classes.container}>
-                En cours d'écriture lol
-            </div>
-        );
-    };
+		this.loadCreneau = this.loadCreneau.bind(this);
+	}
+
+	componentDidMount(){
+		this.loadCreneau();
+		console.log(notation_perm_soir);
+
+	}
+
+
+	loadCreneau(){
+		ajaxGet('perm/creneaux').then(res => {
+			console.log(res.data) 
+
+			this.setState({creneaux: res.data});
+		})
+	}
+
+	render(){
+		
+		const { classes } = this.props;
+		const { creneaux } = this.state;
+		
+
+		return (
+			<div className={classes.container}>
+				
+
+				<Grid container className={classes.section}>
+					<Typography variant="h5" noWrap className={classes.subTitle}>
+						<ChevronRight className={classes.subTitleIcon}/>
+						Notation des perms
+					</Typography>
+					<Grid container className={classes.note}>
+						Ce menu vous permet de noter les perms dont vous avez été astreinteur au cours du semestre.<br/>
+					</Grid>
+
+					<Grid className={classes.section}>
+						<Typography variant="h6" noWrap className={classes.subTitle}>
+							<ChevronRight className={classes.subTitleIcon}/>
+							Perms du matin
+						</Typography>
+						<Grid container>
+							<CarouselItem periode="matin" grid_per_row="2" cell_height="470"/>
+						</Grid>
+					</Grid>
+
+					<Grid className={classes.section}>
+						<Typography variant="h6" noWrap className={classes.subTitle}>
+							<ChevronRight className={classes.subTitleIcon}/>
+							Perms du midi
+						</Typography>
+
+						<Grid container>
+							<CarouselItem periode="midi" grid_per_row="1.5" cell_height="500"/>
+						</Grid>
+					</Grid>
+
+					<Grid className={classes.section}>
+						<Typography variant="h6" noWrap className={classes.subTitle}>
+							<ChevronRight className={classes.subTitleIcon}/>
+							Perms du soir
+						</Typography>
+						<div className={classes.gridList}>
+							<CarouselItem periode="soir" grid_per_row="1" cell_height="800"/>
+						</div>
+					</Grid>
+				</Grid>
+				
+			</div>
+		);
+	};
 
 }
 
 const styles = theme => ({
-    container: {
-        padding: 20,
-        margin: 30,
-        marginTop: 100,
-        border: "1.5px solid #B22132",
-    },
+	container: {
+		padding: 20,
+		margin: 30,
+		marginTop: 100,
+		border: "2px solid #B22132",
+	},
+	section:{
+		paddingBottom :70,
+	},
+	paper: {
+		padding: 10
+	},
+	note: {
+		backgroundColor: 'rgba(0,0,0, 0.05)',
+		padding: 10,
+		border: 'thin solid grey',
+		marginTop: 16,
+		marginBottom: 8,
+	},
+	textField: {
+		marginTop: 16,
+		paddingRight: 15,
+		width: "100%",
+	},
+	suggestions: {
+		zIndex: 100,
+		position: 'absolute',
+		maxHeight: 200,
+		overflowY: 'scroll',
+		marginRight: 15,
+	},
+	card: {
+		margin: 30,
+		padding : 10,
+		fontSize : 9
+	},
+	addButton: {
+		marginTop: 16,
+		marginBottom: 8,
+		height: 49,
+		width: "100%",
+	},
+	subTitle: {
+		marginTop: 10,
+		marginBottom: 10,
+	},
+	subTitleIcon: {
+		marginRight: 8,
+		paddingTop: 5,
+	},
+	row: {
+		height: 40,
+	},
+	cell: {
+		paddingTop: 10,
+		paddingBottom: 10,
+	},
+	btn: {
+		marginLeft: 5,
+		marginRight: 5,
+		marginTop: 3,
+	},
+	gridList: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
 });
 
 export default withStyles (styles) (Dashboard)
+
+
+
+
+
+
+
+
