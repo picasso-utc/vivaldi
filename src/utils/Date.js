6d3f29fba9f40cc74995c8a11c8e41d08cd8524f
Unknown <josselin.pennors@hotmail.fr>
2019-10-23 19:10:44 +0200

    Update dAte utils

--- src/utils/Date.js
+++ src/utils/Date.js
@@ -1,6 +1,12 @@
 export function formateToDjangoDate(date) {
-    const day = ("0" + (date.getDate() + 1)).slice(-2);
+    const day = ("0" + (date.getDate())).slice(-2);
     const month_number = ("0" + (date.getMonth() + 1)).slice(-2)
     const year = date.getFullYear();
     return year + "-" + month_number + "-" + day;
+}
+
+export function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
 }
