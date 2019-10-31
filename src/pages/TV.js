d769920b5a448606a67be8608bbfb45e6aae81ac
Unknown <josselin.pennors@hotmail.fr>
2019-10-31 16:59:44 +0100

    fix issue with config

--- src/pages/TV.js
+++ src/pages/TV.js
@@ -46,12 +46,12 @@ class TV extends React.Component {
                 if (!this.state.mode !== mode || !this.state.src !== path) {
                     this.changeState(mode, path);
                 }
-            } else if (current_date.getHours() === 17 && current_date.getMinutes() <= 10){
+            } else if (current_date.getHours() === 16 && current_date.getMinutes() <= 10){
                 const path="/media/barbar.mp4";
                 if (!this.state.mode !== mode || !this.state.src !== path) {
                     this.changeState(mode, path);
                 }
-            } else if (current_date.getHours() === 17 && current_date.getMinutes() <= 20){
+            } else if (current_date.getHours() === 16 && current_date.getMinutes() <= 20){
                 const path="/media/cidre.mp4";
                 if (!this.state.mode !== mode || !this.state.src !== path) {
                     this.changeState(mode, path);
