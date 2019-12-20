3dcc3a4eb358bd61e34dea2460c111ee2aa7ebfa
Unknown <josselin.pennors@hotmail.fr>
2019-12-20 16:21:57 +0100

    Remove TV routes

--- src/App.js
+++ src/App.js
@@ -12,12 +12,6 @@ import Charte from './components/Charte';
 import Menu from './components/Menu';
 import Error404 from './pages/Error404';
 import ProtectedRoute from './utils/ProtectedRoute';
-import TV from './pages/TV';
-import TVContent from './pages/TVContent';
-import TVMenu from './pages/TVMenu';
-import TVDuelBrasseur from './pages/TVDuelBrasseur';
-import TVDuelBrasseur2 from './pages/TVDuelBrasseur2';
-import PermPic from './pages/PermPic';
 import Poll from './pages/Poll';
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
@@ -35,12 +29,6 @@ class App extends React.Component {
 					<Route path="/charte" exact component={Charte}/>
 					<ProtectedRoute path="/poll/:id" exact component={Poll}/>
 					<Route path="/menu" component={Menu} />
-					<Route path="/tv" exact component={TV}/>
-					<Route path="/tv/content" exact component={TVContent}/>
-					<Route path="/tv/menu" exact component={TVMenu}/>
-					<Route path="/tv/duelbrasseur" exact component={TVDuelBrasseur}/>
-					<Route path="/tv/pic" exact component={PermPic}/>
-					<Route path="/tv/duelbrasseur2" exact component={TVDuelBrasseur2}/>
 					<Route component={Error404}/>
 				</Switch>
 			</BrowserRouter>
