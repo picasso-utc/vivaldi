12e3ef75270f27f2f7d1181b5b9d2285a587a84a
Unknown <josselin.pennors@hotmail.fr>
2019-12-27 20:03:32 +0100

    Edit logout handling

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -3,7 +3,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
-import { API_URL } from '../utils/Config';
+import { asset_url } from '../utils/Config';
+import { ajaxGet } from '../utils/Ajax';
 
 class Logout extends React.Component {
 
@@ -12,7 +13,9 @@ class Logout extends React.Component {
 	}
 
 	logoutUser(){
-        window.location = API_URL + "auth/logout";
+		ajaxGet('auth/logout').then(res => {
+			window.location = asset_url('/')
+		})
     }
 
 
