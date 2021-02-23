1ad0fab47b265e3cc7862e1ee19dcca0b7a660b8
Victor Moreliere <mrcola57@gmail.com>
2021-02-23 15:51:57 +0100

    Update configuration for production

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,6 @@
-export const URL = "" // URL de l'API
-export const API_URL = URL + ""
+// export const URL = "https://kraken.picasso-dev.fr/" // URL de l'API
+export const URL = "https://kraken.picasso-utc.fr/" // URL de l'API
+export const API_URL = URL + "api/"
 
 export const PUBLIC_URL = process.env.PUBLIC_URL || '';
 export function asset_url(path) {
